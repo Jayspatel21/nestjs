7683a7b20b19f81ae369660002cd4de398cadfe5
jayspatel <jayspatel2153@gmail.com>
2024-08-05 10:27:06 +0530

    dockerfile

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,6 +9,6 @@ export class AppController {
   getHello(): string {
     const apiPort = this.configService.get<string>('API_PORT');
     const myEnvName = this.configService.get<string>('MY_ENV_NAME');
-    return `Hello, World! API is running on port: ${apiPort} in ${myEnvName} mode.`;
+    return `Hello, World! API is running on port: ${apiPort} in ${myEnvName} mode + change`;
   }
 }
